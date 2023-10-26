9262b8b5f5392bcc8eb7b2205410ae902a8bd296
KrijanMainali <129727849+KrijanMainali@users.noreply.github.com>
2023-10-26 15:45:26 +0545

    Add files via upload

---
+++ day5(operators).py
@@ -0,0 +1,9 @@
+a = 15
+b = 6
+print(a + b) #addition operator
+print(a - b) #subtraction operator
+print(a * b) #multiplication opeartor
+print(a / b) #division operator
+print(a // b) #floor division operator
+print(a % b) #modulo operator
+print(a ** b) #exponential operator
