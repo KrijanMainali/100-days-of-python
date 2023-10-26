5cc24ae4a60cb0f49116bb6dd76a4aec67c3f3b3
KrijanMainali <129727849+KrijanMainali@users.noreply.github.com>
2023-10-26 14:53:57 +0545

    Add files via upload

---
+++ day3(escape sequence,comments,print).py
@@ -0,0 +1,9 @@
+# author = krijan ,this is single line comment
+'''hello krijan
+hello world'''
+# this is multi line comment
+
+print("hello world \nhello krijan") #\n is escape sequence
+print("My name is \"krijan\" \nand i am 20 years old")
+print("hello world",20,30,sep =",",end = ":")
+print(6)
