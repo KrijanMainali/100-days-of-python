b9e8929b119751f1d3b467a6947e346f0a0e8820
KrijanMainali <129727849+KrijanMainali@users.noreply.github.com>
2023-10-26 15:33:33 +0545

    Add files via upload

---
+++ day4(variables and data types).py
@@ -0,0 +1,24 @@
+name = "krijan"
+age = 20
+can_vote = "true"
+d = None
+print("type of name is",type(name))
+print("type pf age is",type(age))
+print("type of can_vote is",type(can_vote))
+print("type of d is",type(d))
+
+a = 1.1
+b = complex(5,6)
+print(a)
+print(b)
+print("type of a is ",type(a))
+print("type of b is ",type(b))
+
+l = [1,2,3,4]
+t = (1,2,3,4,5,6)
+s = {1,1,2,5,3,4}
+print(l)
+print(t)
+print(s)
+print(type(l),type(t),type(s),sep=",")
+
