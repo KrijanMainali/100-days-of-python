df88e5fee33888b5e33ea711db4fb45aff68a58f
KrijanMainali <129727849+KrijanMainali@users.noreply.github.com>
2023-10-27 14:09:26 +0545

    Add files via upload

---
+++ day9(slicing strings).py
@@ -0,0 +1,5 @@
+name = "krijan"
+print(name[0:3])
+print(name[:3]) #python automatically places 0
+print(name[2:6])
+print(name[2:-2])  #incase of negative slicing the negative value is given by length of string - indexing
