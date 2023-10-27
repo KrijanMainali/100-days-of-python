994e10ba9fa16e9b18dfc1fe461ce5edf82045f5
KrijanMainali <129727849+KrijanMainali@users.noreply.github.com>
2023-10-27 13:51:11 +0545

    Add files via upload

---
+++ day8(strings in python).py
@@ -0,0 +1,8 @@
+first_name = "krijan"
+last_name = "Mainali"
+introduction = "My name is \"krijan mainali\" \nand i am 20 years old"
+print(introduction)
+print(first_name[0])
+introduction = """My name is \"krijan mainali\" 
+and i am 20 years old"""
+print(introduction)
